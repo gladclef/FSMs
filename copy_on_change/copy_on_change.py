f171779aa53d42ebd2bceed594475c06c62bf84d
Benjamin Bean <gladclef@users.noreply.github.com>
2022-12-17 15:27:34 -0700

    added option to watch multiple files

--- copy_on_change/copy_on_change.py
+++ copy_on_change/copy_on_change.py
@@ -3,29 +3,41 @@ import shutil
 import time
 from datetime import datetime
 import winsound
+import psutil
 
 def get_mod_time(fname, default=0):
     try:
-        return pathlib.Path(file_a_name).stat().st_mtime
+        return pathlib.Path(fname).stat().st_mtime
     except FileNotFoundError:
         return default
 
 if __name__ == "__main__":
-    file_a_name = "C:/Users/gladc/Documents/School/UNM/ECE_522_codesign/project/vitis/KMeans/Debug/KMeans.elf"
-    file_b_name = "C:/Users/gladc/Documents/School/UNM/ECE_522_codesign/project/BITSTREAMS/current_lab.elf"
-    last_mod_time = get_mod_time(file_a_name)
+    file_pairs = [
+        [ "C:/Users/gladc/Documents/School/UNM/ECE_522_codesign/project/vitis/KMeans/Debug/KMeans.elf",
+          "C:/Users/gladc/Documents/School/UNM/ECE_522_codesign/project/BITSTREAMS/current_lab.elf" ],
+        [ "C:/Users/gladc/Documents/School/UNM/ECE_522_codesign/project/BITSTREAMS/design_1_wrapper.xsa",
+          None ]
+    ]
+    last_mod_times = [0 for fp in file_pairs]
 
     while True:
-        curr_mod_time = get_mod_time(file_a_name, last_mod_time)
+        for i, fp in enumerate(file_pairs):
+            file_a_name, file_b_name = fp
+            curr_mod_time = get_mod_time(file_a_name, last_mod_times[i])
 
-        if curr_mod_time != last_mod_time:
-            while curr_mod_time != last_mod_time:
-                last_mod_time = curr_mod_time
-                time.sleep(0.15)
-                curr_mod_time = get_mod_time(file_a_name, last_mod_time+1)
+            if curr_mod_time != last_mod_times[i]:
+                while curr_mod_time != last_mod_times[i]:
+                    last_mod_times[i] = curr_mod_time
+                    time.sleep(0.15)
+                    curr_mod_time = get_mod_time(file_a_name, last_mod_times[i]+1)
 
-            print(f"({datetime.now()}) Updating file\n    from: {file_a_name}\n    to:   {file_b_name}")
-            shutil.copyfile(file_a_name, file_b_name)
-            winsound.PlaySound('ding.wav', winsound.SND_FILENAME)
+                if file_b_name != None:
+                    print(f"({datetime.now()}) Updating file\n    from: {file_a_name}\n    to:   {file_b_name}")
+                    shutil.copyfile(file_a_name, file_b_name)
+                    winsound.PlaySound('ding.wav', winsound.SND_FILENAME)
+                else:
+                    print(f"({datetime.now()}) File changed: {file_a_name}")
+                    winsound.PlaySound('ding.wav', winsound.SND_FILENAME)
 
+        #print("%02d " % psutil.cpu_percent(1), end="")
         time.sleep(0.25)
